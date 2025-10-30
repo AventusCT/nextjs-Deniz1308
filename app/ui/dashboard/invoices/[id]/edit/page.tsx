640f27403457648631bcf4c73c332242c41cec50
Deniz1308 <179961@student.aventus.nl>
2025-10-30 11:43:49 +0100

    Update page.tsx

--- app/ui/dashboard/invoices/[id]/edit/page.tsx
+++ app/ui/dashboard/invoices/[id]/edit/page.tsx
@@ -2,28 +2,20 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id;
+export default async function Page(props: { params: { id: string } }) {
+  const id = props.params.id;
 
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
 
-  if (!invoice) {
-    return <p>Invoice not found</p>; // voorkomt onverwachte 404
-  }
-
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
           { label: 'Invoices', href: '/dashboard/invoices' },
-          {
-            label: 'Edit Invoice',
-            href: `/dashboard/invoices/${id}/edit`,
-            active: true,
-          },
+          { label: 'Edit Invoice', href: `/dashboard/invoices/${id}/edit`, active: true },
         ]}
       />
       <Form invoice={invoice} customers={customers} />
