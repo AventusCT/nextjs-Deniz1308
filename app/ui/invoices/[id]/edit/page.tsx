dcf19ea339eafd9a1dd4c63b92e1ea10d6c5da54
Deniz1308 <179961@student.aventus.nl>
2025-10-30 11:36:02 +0100

    Update page.tsx

--- app/ui/invoices/[id]/edit/page.tsx
+++ app/ui/invoices/[id]/edit/page.tsx
@@ -1,14 +1,19 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data'
- 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
-  const params = await props.params;
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+
+export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
+
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  if (!invoice) {
+    return <p>Invoice not found</p>; // voorkomt onverwachte 404
+  }
+
   return (
     <main>
       <Breadcrumbs
